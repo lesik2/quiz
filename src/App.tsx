b46ad20305df1d177dc0608a7c207a5f19eb537c
Aliaksei Pukhalski <lesikpuhalskij@gmail.com>
2023-09-30 20:20:32 +0300

    fix: update styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useContext } from 'react';
 import Test from './components/form/Test';
 import { ResultContext } from './context/ResultProvider';
 import Result from './components/form/Result';
+import './styles/global.css';
+
 function App() {
   const {result} = useContext(ResultContext);
   return (
